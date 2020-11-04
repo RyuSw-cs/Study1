fce41eeab6076e3a50f29278a2ca87635f5fc1e7
Ryu-Sw <rsw1452@gmail.com>
2020-11-05 06:02:29 +0900

    -2초후 다른 레이아웃으로 변경까지

---
+++ Study_Nov1_Homework/app/src/main/java/com/example/study_nov1_homework/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.study_nov1_homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    Handler handle = null;
+    //다른 액티비티를 넘어갈때 사용되는 intent
+    Intent intent = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        handle = new Handler();
+        intent = new Intent(this, move_square.class);
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
---
+++ Study_Nov1_Homework/app/src/main/java/com/example/study_nov1_homework/move_square.java
@@ -0,0 +1,14 @@
+package com.example.study_nov1_homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class move_square extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.move_square);
+    }
+}
